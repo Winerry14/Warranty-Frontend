43f90d88760f9e546b6cbc7b6d043c2ab65b297a
Mirko Nuñez <ma.nbulnes@gmail.com>
2025-11-21 19:38:42 -0300

    agrega corsconfig

---
+++ CorsConfig.java
@@ -0,0 +1,20 @@
+package cl.mirko.warranty.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://warranty-frontend-tpub.onrender.com"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
